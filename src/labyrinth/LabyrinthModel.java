743d82b750d6fd8f7c9446d3782746bc0fc2be6f
cornelius5241 <uucorneliu@gmail.com>
2015-03-19 20:02:29 +0200

    first commit

---
+++ src/labyrinth/LabyrinthModel.java
@@ -0,0 +1,7 @@
+package labyrinth;
+
+/**
+ * Created by cornelius on 3/19/15.
+ */
+public interface LabyrinthModel {
+}
---
+++ src/labyrinth/Main.java
@@ -0,0 +1,11 @@
+package labyrinth;
+
+/**
+ * Created by cornelius on 3/19/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
